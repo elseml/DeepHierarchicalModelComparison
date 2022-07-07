32bb3f203e552e62e813146ad2b073d30cd05025
elseml <60779710+elseml@users.noreply.github.com>
2022-07-07 12:55:50 +0200

    streamline simulation code (remove unnecessary parts)

--- src/julia/04_datasets.jl
+++ src/julia/04_datasets.jl
@@ -15,7 +15,7 @@ function generate_levy_dataset_by_model(model::Int, n_clusters::Int64, n_trials:
             sz = sv = st = 0.0 
         end
 
-        data[k, :, :] = generate_levy_conditions(n_trials, a_l, zr_l, v0_l, v1_l, t0_l, alpha_l, sz, sv, st)
+        data[k, :, :] = generate_levy_participant(n_trials, a_l, zr_l, v0_l, v1_l, t0_l, alpha_l, sz, sv, st)
     end
 
     return data
@@ -34,27 +34,3 @@ function generate_levy_batch(model::Int64, batch_size::Int64, n_clusters::Int64,
 
     return index_list, data
 end
-
-
-# half-finished drafting for a joint function that returns a single datasets from all models. 
-# Separate approach seems better right now in order to split long computation times into smaller parts.
-
-#"Generates a batch of datasets simulated from all models."
-#function generate_levy_batch_all_models(batch_size::Int64, n_clusters::Int64, n_trials::Int64)::Array{Float64, 4}
-#
-#    if batch_size%4 != 0
-#        throw(ArgumentError("batch_size needs to be dividable by 4 without remainder."))
-#    end
-#
-#    data = fill(0.0, (batch_size, n_clusters, n_trials, 3))
-#
-#    datasets_per_model = Int(batch_size/4)
-#    base_model_index = repeat([1,2,3,4], inner=Int(datasets_per_model))
-#
-#    Threads.@threads for m in 1:4
-#        Threads.@threads for d in 1:datasets_per_model
-#            data[d, :, :, :] = generate_levy_dataset_by_model(base_model_index, n_clusters)
-#    end
-
-#    return data
-#end
