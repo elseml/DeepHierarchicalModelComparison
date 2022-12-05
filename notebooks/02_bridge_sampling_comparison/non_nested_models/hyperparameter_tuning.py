d073b291a3568b79d933a3c7385f6bddcf7e2045
elseml <60779710+elseml@users.noreply.github.com>
2022-12-05 15:37:04 +0100

    finish testing setup for dropout and input noise

--- notebooks/02_bridge_sampling_comparison/non_nested_models/hyperparameter_tuning.py
+++ notebooks/02_bridge_sampling_comparison/non_nested_models/hyperparameter_tuning.py
@@ -1,38 +1,37 @@
 import os, sys
 
 # When on cloud (RStudio server)
-sys.path.append(os.path.abspath(os.path.join('../../..'))) # access sibling directories; as in .ipynb
+#sys.path.append(os.path.abspath(os.path.join('../../..'))) # access sibling directories; as in .ipynb
 
 # When on desktop (requires different paths than cloud)
-#sys.path.append(os.path.abspath(os.path.join('../Coding'))) # access sibling directories; different than .ipynb
-#sys.path.append("C:\\Users\\lasse\\Documents\\GitHub\\BayesFlow")
+sys.path.append(os.path.abspath(os.path.join('../Coding'))) # access sibling directories; different than .ipynb
+sys.path.append("C:\\Users\\lasse\\Documents\\GitHub\\BayesFlow")
 
 from src.python.settings import summary_meta_validation, evidence_meta_validation
 from src.python.networks import HierarchicalInvariantNetwork, EvidentialNetwork
 from src.python.models import HierarchicalSdtMptSimulator, MainSimulator
-from src.python.losses import multi_task_loss
+from src.python.losses import softmax_loss
 from src.python.helpers import n_clust_obs_f_f, get_multiple_predictions, get_preds_and_bfs
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.experimental import CosineDecayRestarts
+from tensorflow.keras.experimental import CosineDecay
 from tensorflow.keras.optimizers import Adam
+
 from functools import partial
 
 from bayesflow.trainers import ModelComparisonTrainer
 from bayesflow.amortizers import MultiModelAmortizer
-from bayesflow.losses import log_loss
 
 # For testing: n_runs_per_setting = 1, epochs=2, iterations_per_epoch=2
 # For running: n_runs_per_setting = 5, epochs=15, iterations_per_epoch=1000
 
 #### Runs per setting combination 
-n_runs_per_setting = 1
+n_runs_per_setting = 5
 
 ### Hyperparameter tuning params (cosine decay w/ restarts)
 dropout_list = [False, True]
-input_noise_list = [False, True]
+input_noise_list = [None, 0.05]
 
 n_combinations = len(dropout_list)*len(input_noise_list)*n_runs_per_setting
 
@@ -56,12 +55,17 @@ n_clusters = 25
 n_obs = 50
 
 # Storage path
-file_path = os.path.join(os.getcwd(),'notebooks\\02_bridge_sampling_comparison\\non_nested_models\\hyperparameter_tuning_results')
+file_path = os.path.join(os.getcwd(),'notebooks\\02_bridge_sampling_comparison\\non_nested_models\\hyperparameter_tuning_results_dropout_input_noise')
 
 ### Run tuning
 for dropout in dropout_list:
     for input_noise in input_noise_list:
         for run in range(n_runs_per_setting):
+
+            if dropout == True:
+                evidence_meta_validation.update({'dropout': True})
+            if dropout == False:
+                evidence_meta_validation.update({'dropout': False})
     
             # Initialize from scratch for each run
             summary_net = HierarchicalInvariantNetwork(summary_meta_validation)
@@ -75,14 +79,15 @@ for dropout in dropout_list:
                 network=amortizer, 
                 generative_model=simulator, 
                 loss=partial(softmax_loss),
-                optimizer=partial(Adam, lr_schedule_restart),
+                optimizer=partial(Adam, lr_schedule),
                 skip_checks=True,
                 )
     
             # Train
             losses = trainer.train_online(
                 epochs=1, iterations_per_epoch=1, batch_size=32, 
-                n_obs=partial(n_clust_obs_f_f, n_clusters, n_obs), n_vars=2
+                n_obs=partial(n_clust_obs_f_f, n_clusters, n_obs), n_vars=2,
+                add_noise_std=input_noise
                 )
     
             # Get running loss of final epoch
